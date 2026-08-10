48cb3e8412f021693ad36363f69db784d18da694
Yukkku <yukku.scratcher@gmail.com>
2026-08-10 18:41:13 +0900

    手入れ

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
   outputs =
     { nixpkgs, ... }:
@@ -9,7 +9,6 @@
       eachSystem = lib.genAttrs [
         "aarch64-darwin"
         "aarch64-linux"
-        "i686-linux"
         "x86_64-darwin"
         "x86_64-linux"
       ];
