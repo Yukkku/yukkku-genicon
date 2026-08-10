b719d4c15c5f6cc7b6db2b090b7befc7ac482c8b
Yukkku <yukku.scratcher@gmail.com>
2026-08-10 19:43:32 +0900

    overlaysを提供

--- flake.nix
+++ flake.nix
@@ -12,22 +12,31 @@
         "x86_64-darwin"
         "x86_64-linux"
       ];
+      mkIcongen =
+        pkgs:
+        pkgs.rustPlatform.buildRustPackage (finalAtters: {
+          pname = "yukkku-genicon";
+          version = "0.1.0";
+          src = ./.;
+          cargoHash = "sha256-Fay9evORbfoc/lmBtWiMoMURWhp9F9GhLpY+xC4KJdY=";
+        });
     in
     {
       packages = eachSystem (
         system:
         let
           pkgs = import nixpkgs { inherit system; };
+          yukkku-icongen = mkIcongen pkgs;
         in
         {
-          default = pkgs.rustPlatform.buildRustPackage (finalAtters: {
-            pname = "yukkku-genicon";
-            version = "0.1.0";
-            src = ./.;
-            cargoHash = "sha256-Fay9evORbfoc/lmBtWiMoMURWhp9F9GhLpY+xC4KJdY=";
-          });
+          inherit yukkku-icongen;
+          default = yukkku-icongen;
         }
       );
+      overlays.default = final: _prev: {
+        yukkku-icongen = mkIcongen final;
+      };
+
       devShells = eachSystem (
         system:
         let
